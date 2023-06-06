39803da80368979758ed0b24fc8d60a007689e5d
MyName <myname@gmail.com>
2023-06-06 10:36:24 -0700

    task 14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    print(chr(i), end='')
+# 100-print_tebahpla.py
 
-print()  # Print a new line after the loop ends
-
+""""Print the alphabet in reverse order alternating upper- and lower-case."""
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
