bd703eb40b326800562f33d0bcbaccec3755c9b2
MyName <myname@gmail.com>
2023-06-26 17:54:41 -0700

    task

--- 0x05-python-exceptions/100-main.py
+++ 0x05-python-exceptions/100-main.py
@@ -10,7 +10,7 @@ if not has_been_print:
 value = -89
 has_been_print = safe_print_integer_err(value)
 if not has_been_print:
-    print("{} is not an integer".format(value))
+    print("{} is not an integer".format(value), end='')
 
 value = "School"
 has_been_print = safe_print_integer_err(value)
