e9df75f354498d5c437784ac2b5c2c4e6996cb0a
MyName <myname@gmail.com>
2023-06-26 18:01:56 -0700

    100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,9 +3,8 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        formatted_value = "{:d}".format(value)
-        print(formatted_value)
+        print("{:d}".format(value))
         return True
-    except ValueError:
-        print("Exception: Unknown format code 'd' for object of type '{}'\n".format(type(value).__name__), file=sys.stderr)
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
         return False
