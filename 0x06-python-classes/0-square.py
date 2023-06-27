e801f84a3c354033361065c02521194b01fd7dcc
MyName <myname@gmail.com>
2023-06-27 21:54:39 +0000

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+"""
+This module defines the Square class.
+"""
+
+
 class Square:
+    """
+    This class represents a square.
+    """
     pass
+
+
+if __name__ == "__main__":
+    my_square = Square()
+    print(type(my_square))
+    print(my_square.__dict__)
