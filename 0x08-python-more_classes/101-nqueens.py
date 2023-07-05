f599976f838a0032385ec3e79485e9005047d562
MyName <myname@gmail.com>
2023-07-05 16:01:04 -0700

    sssd

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -8,7 +8,7 @@ def is_safe(board, row, col, N):
     for i in range(row):
         if board[i][col] == 1:
             return False
-
+    
     # Check the upper left diagonal
     i = row - 1
     j = col - 1
@@ -17,7 +17,7 @@ def is_safe(board, row, col, N):
             return False
         i -= 1
         j -= 1
-
+    
     # Check the upper right diagonal
     i = row - 1
     j = col + 1
@@ -26,7 +26,7 @@ def is_safe(board, row, col, N):
             return False
         i -= 1
         j += 1
-
+    
     return True
 
 def place_queens(board, row, N):
@@ -34,14 +34,14 @@ def place_queens(board, row, N):
     if row == N:
         print_solution(board, N)
         return
-
+    
     for col in range(N):
         if is_safe(board, row, col, N):
             board[row][col] = 1  # Place the queen
-
+            
             # Recursively place queens in the next row
             place_queens(board, row + 1, N)
-
+            
             board[row][col] = 0  # Remove the queen (backtrack)
 
 def print_solution(board, N):
@@ -58,19 +58,19 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
+    
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
+    
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-
+    
     # Create an empty chessboard
     board = [[0 for _ in range(N)] for _ in range(N)]
-
+    
     # Solve the N Queens problem
     place_queens(board, 0, N)
