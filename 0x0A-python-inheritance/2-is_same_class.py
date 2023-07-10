c82200b07604a28acc6e40cf30deaa5d4030a1bd
MyName <myemail@gmail.com>
2023-07-10 10:49:55 -0700

    task222

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+""" check if same object """
+
 
 def is_same_class(obj, a_class):
-    """
-    Returns True if the object is exactly an instance of the specified class;
-    otherwise, returns False.
-    """
+    """function check if same class"""
     return type(obj) is a_class
