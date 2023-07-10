f63b79ee144308d5a38237d2609de1c33804d953
MyName <myemail@gmail.com>
2023-07-10 10:04:44 -0700

    task0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-    """Returns a list of available attributes and methods of an object."""
+    """
+    Returns a list of available attributes and methods of an object.
+    """
     return dir(obj)
