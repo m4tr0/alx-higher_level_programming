a671d6b117f7fcd3868a36f889413702eda1f98b
MyName <myemail@gmail.com>
2023-07-11 14:13:52 -0700

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" module """
+
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read(), end='')
+    """ func """
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
