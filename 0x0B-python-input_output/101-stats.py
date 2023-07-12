1f36d185d4765c3530ba125ff930025066d549b8
MyName <myemail@gmail.com>
2023-07-12 13:21:45 -0700

    task fin

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,34 +4,35 @@ import sys
 
 # Initialize variables
 total_file_size = 0
-status_codes = {}
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+line_count = 0
 
 try:
-    line_count = 0
-
     # Read input line by line
     for line in sys.stdin:
         line_count += 1
 
         # Extract relevant information from the input line
         parts = line.split(" ")
-        file_size = int(parts[-1])
+        if len(parts) < 7:
+            continue
+
         status_code = int(parts[-2])
+        file_size = int(parts[-1])
 
         # Compute total file size
         total_file_size += file_size
 
         # Count status codes
-        if status_code not in status_codes:
-            status_codes[status_code] = 1
-        else:
+        if status_code in status_codes:
             status_codes[status_code] += 1
 
         # Print statistics every 10 lines
         if line_count % 10 == 0:
             print("File size:", total_file_size)
-            for code in sorted(status_codes):
-                print(code, ":", status_codes[code])
+            for code, count in sorted(status_codes.items()):
+                if count > 0:
+                    print(code, ":", count)
             print()
 
 except KeyboardInterrupt:
@@ -39,5 +40,6 @@ except KeyboardInterrupt:
 
 # Print final statistics
 print("Total file size:", total_file_size)
-for code in sorted(status_codes):
-    print(code, ":", status_codes[code])
+for code, count in sorted(status_codes.items()):
+    if count > 0:
+        print(code, ":", count)
