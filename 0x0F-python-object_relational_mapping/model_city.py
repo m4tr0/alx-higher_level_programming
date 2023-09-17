dc89ed9ce8b2ea0b339c6e23510ca7896167fe5a
MyName <myemail@gmail.com>
2023-09-17 00:26:42 -0700

    tasks done

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """model state
 """
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base
 
-Base = declarative_base()
 
-
-class State(Base):
+class City(Base):
     """ class class """
-    __tablename__ = 'states'
+    __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,6 +3,7 @@
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -13,3 +14,4 @@ class State(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade="all, delete", backref='state')
